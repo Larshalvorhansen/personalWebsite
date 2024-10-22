b9d30b86a52cc090d313777dea990ab8c024c394
Lars Halvor Hansen <larshalvorhansen1@gmail.com>
2024-10-22 18:15:04 +0200

    BEGIN developement of new color scheme system. Blue! :D

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          white: '#FFFFFF',
+          'rich-black': '#00171F',
+          'prussian-blue': '#003459',
+          cerulean: '#007EA7',
+          'picton-blue': '#00A8E8',
+        },
+        gradientColorStops: theme => ({
+          white: '#FFFFFF',
+          'rich-black': '#00171F',
+          'prussian-blue': '#003459',
+          cerulean: '#007EA7',
+          'picton-blue': '#00A8E8',
+        }),
+      },
+    },
+  }
+  
