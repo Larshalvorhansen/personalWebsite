d48326379d5eb9ed5424874d2103145ee27c6564
larshalvorhansen <larshalvorhansen1@gmail.com>
2025-10-02 17:45:44 +0200

    add db and pca attempt

--- content/eo/PCA/shell.nix
+++ content/eo/PCA/shell.nix
@@ -12,14 +12,17 @@ let
       ipykernel
     ]);
 in pkgs.mkShell {
-  buildInputs = [ py ];
+  buildInputs = [ py pkgs.sqlite ];
 
-  # Cute little sanity ping so you know it worked
   shellHook = ''
+        echo "--- Entering Nix Shell for Data Analysis Project ---"
         echo "Python: $(python --version)"
+        
         python - <<'PY'
     import numpy, pandas, sklearn, matplotlib, seaborn
-    print("Loaded: numpy, pandas, scikit-learn, matplotlib, seaborn")
+    print("Loaded: numpy, pandas, scikit-learn, matplotlib, seaborn.")
     PY
+        
+        echo "The 'sqlite3' command-line tool is also available."
   '';
 }
